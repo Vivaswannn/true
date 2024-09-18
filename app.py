52ea820114eee57da76e38b1ad18dab58ed5d50d
Vivaswannn <vivaswan2002@gmail.com>
2024-09-18 12:38:58 +0530

    my message

---
+++ app.py
@@ -0,0 +1,11 @@
+# Basic Python program to add two numbers
+
+# Input: Asking the user for two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Process: Adding the two numbers
+sum = num1 + num2
+
+# Output: Displaying the result
+print("The sum of", num1, "and", num2, "is:", sum)
